1bc97925522e06f0cf6beb7a2bc6ebf8f575f390
Brezelkoenig <ritzmann@uni-hildesheim.com>
2021-03-17 14:22:47 +0100

    change location of reflexionbutton

--- src/components/ReflexionAuswahl.vue
+++ src/components/ReflexionAuswahl.vue
@@ -1,20 +1,14 @@
 <template>
-                <b-col cols="8" class="mitte border border-primary">
-                    <router-view></router-view>
-                   
-                    
-
-                    <b-dropdown text="Zur Reflexion" class="m-2">
-                    <b-dropdown-item link :to="{name: 'Reflexion'}">Gruppe bilden</b-dropdown-item>
-                    <b-dropdown-item link :to="{name: 'Reflexion'}">Ziel & Umfang definieren</b-dropdown-item>
-                    <b-dropdown-item link :to="{name: 'Reflexion'}">Konzepte & Definitionen</b-dropdown-item>
-                    <b-dropdown-item link :to="{name: 'Reflexion'}">Literatur suchen</b-dropdown-item>
-                    <b-dropdown-item link :to="{name: 'Reflexion'}">Daten extrahieren</b-dropdown-item>
-                    <b-dropdown-item link :to="{name: 'Reflexion'}">Analyse & Synthese</b-dropdown-item>
-                    <b-dropdown-item link :to="{name: 'Reflexion'}">Ergebnisse kommunizieren</b-dropdown-item>
-                    <b-dropdown-item link :to="{name: 'Reflexion'}">Gruppe auflösen</b-dropdown-item>
-                    
-                    
-                    </b-dropdown>
-                </b-col>
+                <div>
+        <b-dropdown text="Zur Reflexion" class="m-2">
+        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Gruppe bilden'}}">Gruppe bilden</b-dropdown-item>
+        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Ziel & Umfang definieren'}}">Ziel & Umfang definieren</b-dropdown-item>
+        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Konzepte & Definitionen'}}">Konzepte & Definitionen</b-dropdown-item>
+        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Literatur suchen'}}">Literatur suchen</b-dropdown-item>
+        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Daten extrahieren'}}">Daten extrahieren</b-dropdown-item>
+        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Analyse & Synthese'}}">Analyse & Synthese</b-dropdown-item>
+        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Ergebnisse kommunizieren'}}">Ergebnisse kommunizieren</b-dropdown-item>
+        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Gruppe auflösen'}}">Gruppe auflösen</b-dropdown-item>
+        </b-dropdown>
+                </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,30 @@
 <template>
-    <div>
-
-
-      <z-canvas :views="$options.components">
-
-            <ReviewCircle> </ReviewCircle>
-
-        </z-canvas>
-
-        <b-dropdown text="Zur Reflexion" class="m-2">
-        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Gruppe bilden'}}">Gruppe bilden</b-dropdown-item>
-        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Ziel & Umfang definieren'}}">Ziel & Umfang definieren</b-dropdown-item>
-        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Konzepte & Definitionen'}}">Konzepte & Definitionen</b-dropdown-item>
-        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Literatur suchen'}}">Literatur suchen</b-dropdown-item>
-        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Daten extrahieren'}}">Daten extrahieren</b-dropdown-item>
-        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Analyse & Synthese'}}">Analyse & Synthese</b-dropdown-item>
-        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Ergebnisse kommunizieren'}}">Ergebnisse kommunizieren</b-dropdown-item>
-        <b-dropdown-item link :to="{name: 'Reflexion', params: { reflexionsPhase: 'Gruppe auflösen'}}">Gruppe auflösen</b-dropdown-item>
-        </b-dropdown>
-    </div>
+  <div>
+    <z-canvas :views="$options.components">
+      <ReviewCircle> </ReviewCircle>
+    </z-canvas>
+    <ReflexionAuswahl />
+  </div>
 </template>
 
 <script>
-import ReviewCircle from '@/components/ReviewCircle.vue'
+import ReviewCircle from "@/components/ReviewCircle.vue";
+import ReflexionAuswahl from "@/components/ReflexionAuswahl.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    ReviewCircle
+    ReviewCircle,
+    ReflexionAuswahl,
   },
   mounted() {
-            this.$zircle.config({
-                mode: 'mixed',
-                style: {
-                    theme: 'white'
-                }
-            })
+    this.$zircle.config({
+      mode: "mixed",
+      style: {
+        theme: "white",
+      },
+    });
 
-            this.$zircle.setView('ReviewCircle')
-        }
-}
+    this.$zircle.setView("ReviewCircle");
+  },
+};
 </script>
