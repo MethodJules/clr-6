9ae57b1ab7c70cdca2574871f9e75f77b7dbfbba
aylin <ayyilmaz17@t-online.de>
2021-06-20 12:10:14 +0200

    Todo Deadlines im Kalender sichtbar

--- src/components/Kalender.vue
+++ src/components/Kalender.vue
@@ -4,7 +4,7 @@
      <div class="giveAppointment"  >
             
             <div class="card-body text-center">
-                 <b-calendar
+                 <!-- <b-calendar
                                     
                                     size= "sm"
                                     hide-header
@@ -12,17 +12,18 @@
                                     @date="showTodos"
                                     :select-attribute="selectAttribute"
                                     :readonly="readonly"
-                ></b-calendar>
+                ></b-calendar> -->
                 <vc-calendar 
                     
                     :attributes='attributes'
                 />
-                   <b-container>
+                   <!-- <b-container>
                        <b-row v-for="i in listOfToDos" :key="i.idx"> <b-col><b-row>Todo: <b-col>{{getTodo[i]}}, bis : {{getDate[i]}}</b-col></b-row></b-col> </b-row>
+                       <b-card style="width: 11rem;" v-for="i in listOfToDos" :key="i.idx"><b-row> <b-col><b-row>Todo: <b-col>{{getTodo[i]}}, <b-row> bis : <b-col>{{getDate[i]}}</b-col></b-row></b-col></b-row></b-col> </b-row></b-card>
                         <b-card style="width: 11rem;"><b-row> <b-col><b-row>Todo: <b-col>{{getTodo[0]}}, <b-row> bis : <b-col>{{getDate[0]}}</b-col></b-row></b-col></b-row></b-col> </b-row></b-card>
                         <b-card style="width: 11rem;"><b-row> <b-col><b-row>Todo: <b-col>{{getTodo[1]}}, <b-row> bis : <b-col>{{getDate[1]}}</b-col></b-row></b-col></b-row></b-col> </b-row></b-card>
                    
-                    </b-container>
+                    </b-container> -->
                 <!-- <div class="giveTodo" v-for="todo in getTodo" :key="todo.getDate">
                     <b-card> Todo: '{{ getTodo}}', bis: {{getDate}} </b-card>
                 </div> -->
@@ -44,23 +45,28 @@ export default {
         //TodoList
     },
     data(){
+        
         const todos = [
       {
-        description: this.$store.state.todo.todos[0],
+        description: this.$store.state.todo.listOfToDos,
         isComplete: false,
-        dates: this.$store.state.todo.dates[0], // Every Friday
+        dates: this.$store.state.todo.dates,
         color: 'red',
       },
-      {
+      /* {
         description: this.$store.state.todo.todos[1],
         isComplete: false,
-        dates: this.$store.state.todo.dates[1], // Every Friday
+        dates: this.$store.state.todo.dates[1], 
         color: 'red',
-      },
+      }, */
     ];
+    
         return{
+            
             incId: todos.length,
             todos,
+            todosFromList: this.$store.state.todo.todos,
+            dateFromList: this.$store.state.todo.dates,
                 /* attributes: [
                     {
                         key: 'today',
@@ -79,7 +85,31 @@ export default {
     },
     methods:{
         showTodos(){
-            this.date =this.$store.state.todo.listOfToDos.date;
+            let ausgabeTodos =[]
+            let count = 1
+            /* const idx = this.todos.findIndex(t=> t.id === this.todo.id);
+            if(idx >= 0){
+                this.todos.splice(idx, 1);
+            } else{
+                this.todos.push({
+                    id: this.todo.id,
+                    date: this.todo.date
+                })
+            } */
+
+
+            for(let i= 0; i< this.dateFromList.length; i++){
+                for(let j= 0; j< this.todosFromList.length; j++){
+                    ausgabeTodos[i][j] = count;
+                    count ++;
+                }
+            }
+            return ausgabeTodos
+                /* ausgabeTodos.push(this.$store.state.todo.todos[1]);
+                ausgabeTodos.push(this.$store.state.todo.todos[0])
+                return ausgabeTodos */
+            
+            
         }
     },
   /*   mounted () {
@@ -93,10 +123,15 @@ export default {
        
     }, */
     computed:{
+        getCompleteList(){
+            
+            var completeList= this.$store.state.todo.listOfToDos[0].todo
+            
+            return completeList
+        },
         getTodo(){
             
-                var neueTodo= this.$store.state.todo.todos
-                
+                var neueTodo=this.$store.state.todo.todos
                 return neueTodo
         },
         getDate(){
@@ -105,24 +140,30 @@ export default {
         },
         attributes() {
             
+            
       return [
         // Attributes for todos
-        ...this.todos.map(todo => ({
-          dates: todo.dates,
-          
+        
+        ...this.$store.state.todo.listOfToDos.map(todo => ({
+          dates: todo.date,
+         
           dot: {
-            color: todo.color,
+            color: 'red',
             class: todo.isComplete ? 'opacity-75' : '',
           },
           popover: {
             
             label: 
-                todo.description,
+                
+                todo.todo,
+                
             
           },
+          
           customData: todo,
         })),
       ];
+      
     },
     }
 }
