ac94dead4b54a59a6d5bc40945bb9347573a7494
yearmfew <brlylmz23@gmail.com>
2021-09-02 13:00:11 +0200

    small change

--- src/store/modules/inputDocuments.js
+++ src/store/modules/inputDocuments.js
@@ -88,7 +88,8 @@ const actions = {
         // const binaryFileData = await base64FileData.blob();
 
         files.forEach((file)=>{
-            const binaryFileData =   fetch(file).blob();
+            const base64FileData = fetch(files);
+            const binaryFileData = base64FileData.blob();
             let fileData = {name:file.name, binaryFileData:binaryFileData};
             fileDatas.push(fileData);
         })
@@ -98,36 +99,36 @@ const actions = {
         var drupalUserUID = rootState.drupal_api.user.uid
         console.log(drupalUserUID)
 
-        // files.forEach((file)=>{
+        fileDatas.forEach((fileData)=>{
       
-        //     var config = {
-        //         method: 'post',
-        //         url: `https://clr-backend.x-navi.de/jsonapi/media/document/field_media_document`,
-        //         headers: {
-        //             'Accept': 'application/vnd.api+json',
-        //             'Content-Type': 'application/octet-stream',
-        //             'Authorization': rootState.drupal_api.authToken,
-        //             'X-CSRF-Token': `${rootState.drupal_api.csrf_token}`,
-        //             'Content-Disposition': 'file; filename="' + file.name + '"',
+            var config = {
+                method: 'post',
+                url: `https://clr-backend.x-navi.de/jsonapi/media/document/field_media_document`,
+                headers: {
+                    'Accept': 'application/vnd.api+json',
+                    'Content-Type': 'application/octet-stream',
+                    'Authorization': rootState.drupal_api.authToken,
+                    'X-CSRF-Token': `${rootState.drupal_api.csrf_token}`,
+                    'Content-Disposition': 'file; filename="' + fileData.name + '"',
 
-        //         },
-        //         data: fetch(file).blob()
+                },
+                data: fileData.binaryFileData
 
-        //     };
+            };
 
-        //     axios(config)
-        //         .then(function (response) {
-        //             console.log(response);
-        //             //commit('SAVE_FILES', { file });
-        //             const documentID = response.data.data.id;
-        //             console.log(documentID)
-        //             dispatch('addInputDocument', documentID)
+            axios(config)
+                .then(function (response) {
+                    console.log(response);
+                    //commit('SAVE_FILES', { file });
+                    const documentID = response.data.data.id;
+                    console.log(documentID)
+                    dispatch('addInputDocument', documentID)
 
-        //         })
-        //         .catch(function (error) {
-        //             console.log(error)
-        //         })
-        // })
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })
+        })
 
     },
 
@@ -183,6 +184,8 @@ const actions = {
 
     },
 
+    // load data von datenbank (action)
+    // speichere die data/files zu input variable (commit)
 
 }
 
