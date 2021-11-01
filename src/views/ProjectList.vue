2f516fd71cc1f98100cc214445a1a6e58b982e35
Brezelkoenig <ritzmann@uni-hildesheim.de>
2021-11-01 18:54:14 +0100

    projectlist fix

--- src/views/ProjectList.vue
+++ src/views/ProjectList.vue
@@ -1,34 +1,31 @@
 <template>
-    <div>
-        <h1>Meine Projekte</h1>
-        <br />
-        <b-row>
-            <!-- <b-card class="m-2"
+  <div>
+    <h1>Meine Projekte</h1>
+    <br />
+    <b-row>
+      <!-- <b-card class="m-2"
 
         v-for="proj in projectList"
         :key="proj.projectId"
         :title="proj.titel"
       > -->
 
+      <div>
+        <b-row>
+          <b-col>
             <div>
-                <b-row>
+              <table>
+                <tr v-for="project in getMyProjectlist" :key="project.uuid">
+                  <b-card style="max-height: 20rem">
                     <b-col>
-                        <div>
-                            <table>
-                                <tr
-                                    v-for="project in getMyProjectlist"
-                                    :key="project.uuid"
-                                >
-                                    <b-card style="max-height: 20rem">
-                                        <b-col>
-                                            <b-row>
-                                                <b-col>
-                                                    <h3>{{ project.title }}</h3>
-                                                </b-col>
-                                            </b-row>
-                                            <b-row>
-                                                <b-col>
-                                                    <!--                         <b-link
+                      <b-row>
+                        <b-col>
+                          <h3>{{ project.title }}</h3>
+                        </b-col>
+                      </b-row>
+                      <b-row>
+                        <b-col>
+                          <!--                         <b-link
 
                           :to="{ name: 'Home', params: { user_id: getUserID } }"
                           class="btn btn-outline-dark btn-block mb-2"
@@ -36,108 +33,101 @@
                         >
                       </b-col> -->
 
-                                                    <b-link
-                                                        :to="{
-                                                            name: 'Home',
-                                                            params: {
-                                                                project_id:
-                                                                    project.uuid,
-                                                            },
-                                                        }"
-                                                        class="
-                                                            btn
-                                                            btn-outline-dark
-                                                            btn-block
-                                                            mb-2
-                                                        "
-                                                        >Dashboard</b-link
-                                                    >
-                                                </b-col>
-                                            </b-row>
-                                            <b-row>
-                                                <b-col>
-                                                    <ReflexionAuswahl
-                                                        :projectId="
-                                                            project.uuid
-                                                        "
-                                                    />
-                                                </b-col>
-                                            </b-row>
-                                        </b-col>
-                                    </b-card>
-                                </tr>
-                            </table>
-                        </div>
+                          <b-link
+                            :to="{
+                              name: 'Home',
+                              params: {
+                                project_id: project.uuid,
+                              },
+                            }"
+                            class="btn btn-outline-dark btn-block mb-2"
+                            >Dashboard</b-link
+                          >
+                        </b-col>
+                      </b-row>
+                      <b-row>
+                        <b-col>
+                          <ReflexionAuswahl :projectId="project.uuid" />
+                        </b-col>
+                      </b-row>
                     </b-col>
-                </b-row>
+                  </b-card>
+                </tr>
+              </table>
             </div>
-            <!-- </b-card> -->
-
-            <b-card title="Neues Projekt" style="max-height: 10rem" class="m-2">
-                <b-row>
-                    <b-col cols="3">
-                        <ProjectForm :project="project"></ProjectForm>
-
-                        <!-- <b-link class=" btn btn-outline-dark mt-5">Neues Projekt</b-link> -->
-                    </b-col>
-                </b-row>
-            </b-card>
+          </b-col>
         </b-row>
-    </div>
+      </div>
+      <!-- </b-card> -->
+
+      <b-card title="Neues Projekt" style="max-height: 10rem" class="m-2">
+        <b-row>
+          <b-col cols="3">
+            <ProjectForm :project="project"></ProjectForm>
+
+            <!-- <b-link class=" btn btn-outline-dark mt-5">Neues Projekt</b-link> -->
+          </b-col>
+        </b-row>
+      </b-card>
+    </b-row>
+  </div>
 </template>
 
 <script>
 import ReflexionAuswahl from "@/components/ReflexionAuswahl.vue";
 import ProjectForm from "@/components/ProjectForm";
 export default {
-    name: "ProjectList",
+  name: "ProjectList",
 
-    components: {
-        ReflexionAuswahl,
-        ProjectForm,
+  components: {
+    ReflexionAuswahl,
+    ProjectForm,
+  },
+  data() {
+    return {
+      project: {
+        kurzbeschreibung: "",
+        betreuenderDozent: [""],
+        externeMitwirkende: "",
+        schlagworter: [],
+        uuid: "",
+        title: "",
+      },
+
+      //projectList: this.getMyProjectlist,
+    };
+  },
+
+  methods: {
+    fetchData(proj) {
+      this.project.title = proj.title;
     },
-    data() {
-        return {
-            project: {
-                kurzbeschreibung: "",
-                betreuenderDozent: [""],
-                externeMitwirkende: "",
-                schlagworter: [],
-                uuid: "",
-                title: "",
-            },
+  },
 
-            //projectList: this.getMyProjectlist,
-        };
-    },
-
-    methods: {
-        fetchData(proj) {
-            this.project.title = proj.title;
-        },
-    },
-
-    computed: {
-        getCurrentUserInternalUID() {
-            // return true
-            return this.$store.state.drupal_api.user.uid;
-        },
-
-        getLoadingStatus() {
-            return this.$store.state.loadingStatus;
-        },
+  computed: {
+    getCurrentUserInternalUID() {
+      // return true
+      return this.$store.state.drupal_api.user.uid;
     },
 
-    async mounted() {
-        this.$store.dispatch("project/loadProjectsFromBackend").then(() => {});
-
-        //load lecturer student and user for different lists
-        this.$store.dispatch("user/loadLecturersFromBackend");
-        this.$store.dispatch("user/loadStudentsFromBackend");
-        this.$store.dispatch(
-            "profile/loadUserFromBackend",
-            this.getCurrentUserInternalUID
-        );
+    getLoadingStatus() {
+      return this.$store.state.loadingStatus;
     },
+    getMyProjectlist() {
+      return this.$store.state.project.myProjects;
+    },
+  },
+
+  async mounted() {
+    this.$store.dispatch("project/loadProjectsFromBackend").then(() => {});
+
+    //load lecturer student and user for different lists
+    this.$store.dispatch("user/loadLecturersFromBackend");
+    this.$store.dispatch("user/loadStudentsFromBackend");
+    this.$store.dispatch(
+      "profile/loadUserFromBackend",
+      this.getCurrentUserInternalUID
+    );
+  },
 };
 </script>
